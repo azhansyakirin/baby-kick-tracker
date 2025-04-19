783dc29f4612ee4146c9a65729dc511911f892f9
Azhan Syakirin <azhansyakirin@gmail.com>
2025-04-19 10:34:56 +0800

    Change image for kick

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,14 +3,14 @@
 export const Button = ({ onClick }) => {
     return (
         <button
-            className="rounded-full size-64 active:scale-95 border-[var(--primary)] border-2 cursor-pointer flex justify-center items-center p-8 hover:shadow-xl transition-all ease-in-out duration-300"
+            className="rounded-full size-64 active:scale-95 border-[var(--text-primary)] border-2 cursor-pointer flex justify-center items-center p-8 hover:shadow-xl transition-all ease-in-out duration-300"
             onClick={onClick}
         >
             <img
-                src="/feet.webp"
+                src="/feet-a47148.webp"
                 className="w-full h-full object-contain overflow-hidden"
                 alt="Press Here"
-                title="Press Here"
+                title="Kicking!"
                 draggable="false"
                 loading="lazy"
             />
