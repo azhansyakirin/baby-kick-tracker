98938c33582a7cab3865c93e72a52037b808992a
Azhan Syakirin <azhansyakirin@gmail.com>
2025-06-08 03:54:23 +0800

    Private Layout to handle private route

---
+++ src/components/Layout/PrivateLayout.jsx
@@ -0,0 +1,30 @@
+import { useLocation } from "react-router-dom";
+import { useAuth } from "../../Context/AuthContext"
+import { Sidebar } from "../Sidebar"
+
+export const PrivateLayout = ({ children }) => {
+
+  const { setUser } = useAuth();
+  const location = useLocation();
+
+  const pageMap = {
+    '/home': 'Home',
+    '/baby': 'Your Baby',
+    '/appointments': 'Your Appointments',
+    '/records': 'Your Records',
+    '/journey': 'Your Journey',
+    '/': 'Page Title'
+  }
+
+  return (
+    <div className="flex flex-row max-w-screen overflow-hidden box-border min-h-screen">
+      <Sidebar handleLogoutUser={() => setUser(null)} />
+      <main className="flex-1 md:ml-16 p-4 box-content min-w-0 overflow-x-auto">
+        <div>
+          <h1 className="font-[Inter] text-2xl md:text-4xl font-bold tracking-tight">{pageMap[location.pathname || '/']}</h1>
+        </div>
+        {children}
+      </main>
+    </div>
+  )
+}
