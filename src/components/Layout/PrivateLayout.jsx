1a9323b3ad44b494141b7e0e045398ac61981e9d
Azhan Syakirin <azhansyakirin@gmail.com>
2025-06-15 22:37:21 +0800

    add watermark to layout

--- src/components/Layout/PrivateLayout.jsx
+++ src/components/Layout/PrivateLayout.jsx
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import { useAuth } from "../../Context/AuthContext"
 import { Sidebar } from "../Sidebar"
 import { Loader } from "../Loader";
+import { Watermark } from "../Watermark";
 
 export const PrivateLayout = ({ children, loading }) => {
 
@@ -19,13 +20,14 @@ export const PrivateLayout = ({ children, loading }) => {
 
   return (
     <div className="flex flex-row max-w-screen overflow-hidden box-border min-h-screen">
-      {loading && <Loader/>}
+      {loading && <Loader />}
       <Sidebar handleLogoutUser={() => setUser(null)} />
       <main className="flex-1 md:ml-16 p-4 box-content min-w-0 overflow-x-auto">
         <div>
           <h1 className="font-[Inter] text-2xl md:text-4xl font-bold tracking-tight">{pageMap[location.pathname || '/']}</h1>
         </div>
         {children}
+        <Watermark />
       </main>
     </div>
   )
